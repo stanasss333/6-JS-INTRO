0a9acb80acf4fc774fb7bc0ecf6cf7cf3a5806b1
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-09 20:12:31 +0200

    Intro

---
+++ js/01-variables.js
@@ -0,0 +1,43 @@
+console.log('Labas rytas, Lietuva');
+const pasisveikinimas = 'Labas rytas, Lietuva';
+console.log(pasisveikinimas);
+
+const pirmasSkaicius = 4;
+const antrasSkaicius = 44;
+const suma = pirmasSkaicius + antrasSkaicius;
+
+console.log(pirmasSkaicius);
+console.log(antrasSkaicius);
+console.log(suma);
+
+console.log(pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+
+const mathText = pirmasSkaicius + ' + ' + antrasSkaicius + ' = ' + suma;
+console.log(mathText);
+
+const petrasName = 'Petras';
+const age = 99;
+const petrasAge = petrasName + ' age is ' + age + '.';
+console.log(petrasAge);
+
+const pazymiai = [5, 7, 8, 3, 10, 7];
+console.log(pazymiai);
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f' ];
+console.log(abc);
+
+const pirmasPazymys = pazymiai[0];
+console.log(pirmasPazymys);
+
+const antrasPazymys = pazymiai[1];
+console.log(antrasPazymys);
+
+const pazymiuSuma = pazymiai[0] + pazymiai[1] + pazymiai[2] + pazymiai[3] + pazymiai[4] + pazymiai[5];
+
+const pazymiuKiekis = pazymiai.length;
+
+const pazymiuVidurkis = pazymiuSuma / pazymiuKiekis;
+
+console.log('Pazymiu suma:', pazymiuSuma);
+console.log('Pazymiu kiekis:', pazymiuKiekis);
+console.log('Pazymiu vidurkis:', pazymiuVidurkis);
