48e10fdbbf369bb9f2868985ffbc6d9623b67086
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-09 21:07:25 +0200

    Kintamuju iniciavimas

---
+++ js/02-kintamuju iniciavimas.js
@@ -0,0 +1,28 @@
+const treciasSkaicius = 5;
+const ketvirtasSkaicius = 6;
+const penktasSkaicius = 8;
+
+console.log(treciasSkaicius);
+console.log(ketvirtasSkaicius);
+console.log(penktasSkaicius);
+
+const skaicius = 'devyni';
+console.log(skaicius);
+const skaiciusA = 'astuoni';
+console.log(skaiciusA);
+const skaiciusB = 'sesi';
+console.log(skaiciusB);
+
+const skaiciai = [1, 2, 3, 4, 5];
+console.log(skaiciai);
+const automobiliai = [6, 7, 8, 9, 10];
+console.log(automobiliai);
+const vazonai = [11, 12, 13, 14, 15];
+console.log(vazonai);
+
+const dgj = ['d', 'e', 'f', 'g', 'j'];
+console.log(dgj);
+const zodynai = ['lietuviu', 'anglu', 'rusu', 'italu', 'prancuzu'];
+console.log(zodynai);
+const geles = ['roze', 'herbera', 'tulpe', 'bijunas', 'amarilis'];
+console.log(geles);
