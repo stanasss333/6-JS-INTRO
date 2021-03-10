920a184f95b14c3ff6394fc9529306ae27fa3e14
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-10 16:13:31 +0200

    New veiksmai su kintamaisiais

--- js/03-veiksmai su kintamaisiais.js
+++ js/03-veiksmai su kintamaisiais.js
@@ -11,9 +11,9 @@ console.log('Automobiliu suma:', automobiliuSuma);
 const vazonuSuma = vazonai[1] - vazonai[2] + vazonai[3] - vazonai[4] + vazonai[5];
 console.log('Vazonu suma:', vazonuSuma);
 
-const dgjSuma = dgj[4] + ',' + dgj[3] + ',' + dgj[2] + ',' + dgj[1] + ',' + dgj[0];
+const dgjSuma = dgj[4] + ', ' + dgj[3] + ', ' + dgj[2] + ', ' + dgj[1] + ', ' + dgj[0];
 console.log('Dgj suma:', dgjSuma);
-const zodynuSuma = zodynai[4] + ',' + zodynai[3] + ',' + zodynai[2] + ',' + zodynai[1] + ',' + zodynai[0];
+const zodynuSuma = zodynai[4] + ', ' + zodynai[3] + ', ' + zodynai[2] + ', ' + zodynai[1] + ', ' + zodynai[0];
 console.log('Zodynu suma:', zodynuSuma);
-const geliuSuma = geles[4] + ',' + geles[3] + ',' + geles[2] + ',' + geles[1] + ',' + geles[0];
+const geliuSuma = geles[4] + ', ' + geles[3] + ', ' + geles[2] + ', ' + geles[1] + ', ' + geles[0];
 console.log('Geliu suma:', geliuSuma);
