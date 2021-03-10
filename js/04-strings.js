6edadebfb2f1452d1e8f4a1fbb8f545e7871a237
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-10 18:13:47 +0200

    Stringai

---
+++ js/04-strings.js
@@ -0,0 +1,54 @@
+const menuo = 'Kovo';
+const diena = 'desimta';
+const dayString = 'Siandien yra: ' + menuo + ' ' + diena + '.';
+console.log(dayString);
+
+const dvigubaKabute = 'Cia yra dviguba (") kabute.';
+console.log(dvigubaKabute);
+
+const viengubaKabute = "Cia yra vienguba (') kabute.";
+console.log(viengubaKabute);
+
+const viengubaDviguba = "Turiu vienguba (') ir dviguba (\") kabutes.";
+console.log(viengubaDviguba);
+
+const dvigubaVienguba = 'Turiu dviguba (") ir vienguba (\') kabutes.';
+console.log(dvigubaVienguba);
+
+const maxKabutes1 = 'Max saugumas dviguba (\") ir vienguba (\') kabutes.';
+console.log(maxKabutes1);
+
+const maxKabutes2 = "Max saugumas dviguba (\") ir vienguba (\') kabutes.";
+console.log(maxKabutes2);
+
+const navHTML = '<nav>\
+                    <a href="#">Link</a>\
+                    <a href="#">Link</a>\
+                    <a href="#">Link</a>\
+                    <a href="#">Link</a>\
+                </nav>';
+console.log(navHTML); 
+
+const vardas = 'Maryte';
+const petrasIntro = 'Mano vardas: ' + vardas + '.';
+console.log(petrasIntro);
+
+const petrasBacktick = `Mano vardas: ${vardas}.`;
+console.log(petrasBacktick);
+
+const marks = [10, 5, 8, 7, 2, 10, 8, 6];
+const markSentence0 = `Pirmas pazymys: ${marks[0]}.`;
+console.log(markSentence0);
+const markSentence1 = `Antras pazymys: ${marks[1]}.`;
+console.log(markSentence1);
+const markSentence = `Naujausias pazymys: ${marks[marks.length - 1]}.`;
+console.log(markSentence);
+
+const navHTML2 = `<nav>
+<a href="#">Link</a>
+<a href="#">Link</a>
+<a href="#">Link</a>
+<a href="#">Link</a>
+</nav>`;
+console.log(navHTML2);
+ 
