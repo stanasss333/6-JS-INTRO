9118b979fd976825234ea96097450ed552787b41
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-13 18:05:51 +0200

    Kintamuju palyginimas

---
+++ js/06-kintamuju-palyginimas.js
@@ -0,0 +1,342 @@
+const e = 5;
+const f = 6;
+const g = 8;
+
+if (e > f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (g > f); {
+    console.log('Pomidoras');
+} 
+
+if (e < f) {
+    console.log('Pomidoras');
+} 
+
+if (g < f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (e === f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (g === f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (e !== f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (g !== f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (e >= f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (e <= f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (g >= f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+if (g <= f) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+const skaicius = 'devyni';
+const skaiciausIlgis = skaicius.length;
+console.log(skaiciausIlgis);
+
+const skaiciusA = 'astuoni';
+const skaiciusAIlgis = skaiciusA.length;
+console.log(skaiciusAIlgis);
+
+const skaiciusB = 'sesi';
+const skaiciusBIlgis = skaiciusB.length;
+console.log(skaiciusBIlgis);
+
+const a = 6;
+const b = 7;
+const c = 4;
+
+if (a > b) {
+    const abDaugiau = `${a} yra daugiau uz ${b}`;
+    console.log(abDaugiau);
+}  else {
+    const abNedaugiau = `${a} yra Ne daugiau uz ${b}`;
+    console.log(abNedaugiau);
+}
+
+if (b > c) {
+    const bcDaugiau = `${b} yra daugiau uz ${c}`;
+    console.log(bcDaugiau);
+} else {
+    const bcNedaugiau = `${b} yra Ne daugiau uz ${c}`;
+    console.log(bcNedaugiau);
+}
+
+if (c > a) {
+    const caDaugiau = `${c} yra daugiau uz ${a}`;
+    console.log(caDaugiau);
+} else {
+    const caNedaugiau = `${c} yra Ne daugiau uz ${a}`;
+    console.log(caNedaugiau);
+}
+
+if (a < b) {
+    const abMaziau = `${a} yra maziau uz ${b}`;
+    console.log(abMaziau);
+} else {
+    const abNeMaziau = `${a} yra Ne maziau uz ${b}`;
+    console.log(abNeMaziau);
+}
+
+if (b < c) {
+    const bcMaziau = `${b} yra maziau uz ${c}`;
+    console.log(bcMaziau);
+} else {
+    const bcNeMaziau = `${b} yra Ne maziau uz ${c}`;
+    console.log(bcNeMaziau);
+}
+
+if (c < a) {
+    const caMaziau = `${c} yra maziau uz ${a}`;
+    console.log(caMaziau);
+} else {
+    const caNeMaziau = `${c} yra Ne maziau uz ${a}`;
+    console.log(caNeMaziau);
+}
+
+if (a === b) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (b === c) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (c === a) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (a !== b) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+if (b !== c) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (c !== a) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (a >= b) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (b >= c) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (c >= a) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (a <= b) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (b <= c) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (c <= a) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+const skaiciai = [1, 2, 3, 4, 5, 6, 7];
+const skaiciaiIlgis = skaiciai.length;
+console.log(skaiciaiIlgis);
+
+const automobiliai = [6, 7, 8, 9, 10, 16];
+const automobiliaiIlgis = automobiliai.length;
+console.log(automobiliaiIlgis);
+
+const vazonai = [11, 12, 13, 14, 15];
+const vazonaiIlgis = vazonai.length;
+console.log(vazonaiIlgis);
+
+const h = 7;
+const j = 6;
+const k= 5;
+
+if (h > j) {
+    const hjDaugiau = `${h} yra daugiau uz ${j}`;
+    console.log(hjDaugiau);
+}  else {
+    const hjNedaugiau = `${h} yra Ne daugiau uz ${j}`;
+    console.log(hjNedaugiau);
+}
+
+if (j > k) {
+    const jkDaugiau = `${j} yra daugiau uz ${k}`;
+    console.log(jkDaugiau);
+} else {
+    const jkNedaugiau = `${j} yra Ne daugiau uz ${k}`;
+    console.log(jkNedaugiau);
+}
+
+if (k > h) {
+    const khDaugiau = `${k} yra daugiau uz ${h}`;
+    console.log(khDaugiau);
+} else {
+    const khNedaugiau = `${k} yra Ne daugiau uz ${h}`;
+    console.log(khNedaugiau);
+}
+
+if (h < j) {
+    const hjMaziau = `${h} yra maziau uz ${j}`;
+    console.log(hjMaziau);
+} else {
+    const hjNeMaziau = `${h} yra Ne maziau uz ${j}`;
+    console.log(hjNeMaziau);
+}
+
+if (j < k) {
+    const jkMaziau = `${j} yra maziau uz ${k}`;
+    console.log(jkMaziau);
+} else {
+    const jkNeMaziau = `${j} yra Ne maziau uz ${k}`;
+    console.log(jkNeMaziau);
+}
+
+if (k < h) {
+    const khMaziau = `${k} yra maziau uz ${h}`;
+    console.log(khMaziau);
+} else {
+    const khNeMaziau = `${k} yra Ne maziau uz ${h}`;
+    console.log(khNeMaziau);
+}
+
+if (h === j) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (j === k) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (k === h) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if ( h !== j) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (j !== k) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (k !== h) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (h >= j) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (j >=k) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (k >= h) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (h <= j) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (j <= k) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+if (k <= h) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
