213c941920ed7aaaf814f56a5e663c07d327d1d2
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-14 13:51:36 +0200

    Ifnd

---
+++ js/07-ifnd.js
@@ -0,0 +1,48 @@
+const saule = 'Sviecia';
+
+if (saule === 'Sviecia') {
+    console.log('Pasiimti kepure');
+} else if (saule === 'Nesviecia') {
+        console.log('Nieko neimti');
+    }
+
+const sauleA = 'Nesviecia';
+
+if (sauleA === 'Sviecia') {
+    console.log('Pasiimti kepure');
+} else if (sauleA === 'Nesviecia') {
+    console.log('Nieko neimti');
+}
+
+const lietus = 'Lyja';
+
+if (lietus === 'Lyja') {
+    console.log('Pasiimti sketi');
+} else if (lietus === 'Nelyja') {
+    console.log('Nieko neimti');
+}
+
+const lietusA = 'Nelyja';
+
+if (lietusA === 'Lyja') {
+    console.log('Pasiimti sketi');
+} else if (lietusA === 'Nelyja') {
+    console.log('Nieko neimti');
+}
+
+const vilkas = 'Stovi';
+
+if (vilkas === 'Stovi') {
+    console.log('I lauka negalima eiti');
+} else if (vilkas === 'Jo nera') {
+    console.log('I lauka galima eiti');
+}
+
+const vilkasA = 'Jo nera';
+
+if (vilkasA === 'Stovi') {
+    console.log('I lauka negalima eiti');
+} else if (vilkasA === 'Jo nera') {
+    console.log('I lauka galima eiti');
+}
+
