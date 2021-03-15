7c9bee5346d941f23867d5c4b3a73086bcf37b07
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-15 17:25:53 +0200

    Math

---
+++ js/10-math.js
@@ -0,0 +1,83 @@
+const pazymiai = [7, 10, 7];
+const suma = pazymiai[0] + pazymiai[1] + pazymiai[2];
+const vidurkis = suma / pazymiai.length;
+console.log(vidurkis);
+
+const marks = [10, 8, 6, 4];
+const count = marks.length
+let sum = 0;
+console.log('Sum:', sum);
+
+sum = sum + marks[0];
+console.log('Sum:', sum);
+
+sum = sum + marks[1];
+console.log('Sum:', sum);
+
+sum = sum + marks[2];
+console.log('Sum:', sum);
+
+sum = sum + marks[3];
+console.log('Sum:', sum);
+
+const average = sum / count;
+console.log(average);
+
+const marks2 = [2, 4, 6, 8];
+let sum2 = 0;
+console.log(sum2);
+
+sum2 += marks2[0];
+console.log(sum2);
+
+sum2 += marks2[1];
+console.log(sum2);
+
+sum2 += marks2[2];
+console.log(sum2);
+
+sum2 += marks2[3];
+console.log(sum2);
+
+const marks3 = [2, 4, 6, 8];
+
+let sum3 = 0;
+let markIndex3 = 0;
+console.log(sum3);
+
+sum3 += marks3[markIndex3]
+console.log(sum3);
+
+markIndex3 += 1;
+sum3 += marks3[markIndex3]
+console.log(sum3);
+
+markIndex3 += 1;
+sum3 += marks3[markIndex3]
+console.log(sum3);
+
+markIndex3 += 1;
+sum3 += marks3[markIndex3]
+console.log(sum3);
+
+const abc = ['r', 'y', 't', 'a', 's'];
+
+let zodis = '';
+let abcIndex = 0;
+
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+
+console.log(zodis);
+
+let a = 5;
+
+console.log(++a);
+console.log(++a);
+console.log(++a);
+console.log(++a);
+console.log(++a);
+console.log(++a);
