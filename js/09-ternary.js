8bd87a270cb6759ac643a1279f2b769e1695b91b
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-15 16:34:21 +0200

    Ternary

---
+++ js/09-ternary.js
@@ -0,0 +1,22 @@
+const a = 5;
+const b = 7;
+
+let ats = '';
+
+if (a > b) {
+    ats = 'daugiau';
+} else {
+    ats = 'ne daugiau';
+}
+console.log(ats);
+
+const c = 3;
+const d = 5;
+
+const ats2 = c > d ? 'daugiau' : 'ne daugiau';
+
+console.log(ats2);
+
+const ats3 = c < d ? 'maziau' : 'ne maziau';
+
+console.log(ats3);
