f2656bd9e8e0fd772523c59af2618ce803ec9628
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-16 19:33:24 +0200

    Marks

---
+++ js/16-marks.js
@@ -0,0 +1,25 @@
+function pazymiuVidurkis (vardas, pazymiai) {
+    let suma = 0;
+
+    for (let i = 0; i < pazymiai.length; i++) {
+        suma += pazymiai[i];
+    }
+    const average = suma / pazymiai.length;
+
+    console.log(`${vardas}: pazymiu vidurkis yra ${average}.`);
+}
+
+const mokykla = [
+    ['Petriukas', [10, 2, 8, 4]],
+    ['Maryte', [10, 8, 6, 4]],
+    ['Alphe', [10, 9, 10, 7, 9, 2, 7]]
+];
+
+for (let i = 0; i < mokykla.length; i++) {
+    const mokinys = mokykla[i];
+    const name = mokinys[0];
+    const marks = mokinys[1];
+
+    pazymiuVidurkis(name, marks);
+    pazymiuVidurkis(mokykla[i][0], mokykla[i][1]);
+}
