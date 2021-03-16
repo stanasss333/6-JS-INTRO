9b11e0113f5f2ed1a3232317198b730742b0c241
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-16 19:47:59 +0200

    Multiplication

---
+++ js/18-daugyba.js
@@ -0,0 +1,9 @@
+function daugyba(a, b) {
+    const rez = a * b;
+    return rez;
+}
+
+console.log(daugyba(4, 5));
+console.log(daugyba(6, 5));
+console.log(daugyba(4, 6));
+console.log(daugyba(3.14, 6));
