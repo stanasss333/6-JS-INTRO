0dbb8c843f59822f9bb10699db9c7518bb962cbf
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-17 16:53:55 +0200

    Daugyba

--- js/18-daugyba.js
+++ js/18-daugyba.js
@@ -1,9 +1,54 @@
 function daugyba(a, b) {
+    if (typeof a !== 'number') {
+        return 'Pirmasis parametras turi buti skaiciaus tipo';
+    }
+
+    if (!isFinite(a)) {
+        return `Pirmasis parametras turi buti normalus skaicius ir negali buti ${a}`
+    }
+
+    if (typeof b !== 'number') {
+        return 'Antrasis parametras turi buti skaiciaus tipo';
+    }
+
+    if (!isFinite(b)) {
+        return `Antrasis parametras turi buti normalus skaicius ir negali buti ${b}`
+    }
     const rez = a * b;
     return rez;
-}
+} 
 
-console.log(daugyba(4, 5));
-console.log(daugyba(6, 5));
-console.log(daugyba(4, 6));
-console.log(daugyba(3.14, 6));
+console.log(daugyba('trys', 'penki'));
+console.log(daugyba(3, 'penki'));
+console.log(daugyba('penki', 3));
+console.log(daugyba(true, false));
+console.log(daugyba(3, false));
+console.log(daugyba(false, 3));
+console.log(daugyba(['asd', 786], 3));
+console.log(daugyba(3, ['asd', 786]));
+console.log(daugyba(['asd', 786], ['asd', 786]));
+console.log(daugyba(['asd'], 3));
+console.log(daugyba(3, ['asd']));
+console.log(daugyba(['asd'], ['asd']));
+console.log(daugyba([2], 3));
+console.log(daugyba(3, [2]));
+console.log(daugyba([2], [2]));
+console.log(daugyba(NaN, 3));
+console.log(daugyba(3, NaN));
+console.log(daugyba(NaN, NaN));
+console.log(daugyba(Infinity, 3));
+console.log(daugyba(3, Infinity));
+console.log(daugyba(Infinity, Infinity));
+console.log(daugyba(-Infinity, 3));
+console.log(daugyba(3, -Infinity));
+console.log(daugyba(-Infinity, -Infinity));
+
+console.log(daugyba(4, 5), '->', 20);
+console.log(daugyba(44, 5), '->', 220);
+console.log(daugyba(-44, -55), '->', 2420);
+console.log(daugyba(-44, 55), '->', -2420);
+console.log(daugyba(-44, 0), '->', 0);
+console.log(daugyba(0, 0), '->', 0);
+console.log(daugyba(0, 54850), '->', 0);
+console.log(daugyba(3.14, 2), '->', 6.28);
+console.log(daugyba(3, 5), '->', 15);
