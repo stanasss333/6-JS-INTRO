263523520c2a46432be814a20030a2e2903a73b3
stanasss333 <79447024+stanasss333@users.noreply.github.com>
2021-03-17 18:00:06 +0200

    century

---
+++ codewars/century.js
@@ -0,0 +1,9 @@
+function century(year) {
+    return Math.ceil(year / 100);
+}
+
+console.log(century(1705), '->', 18);
+console.log(century(1900), '->', 19);
+console.log(century(1601), '->', 17);
+console.log(century(2000), '->', 20);
+console.log(century(89), '->', 1);
